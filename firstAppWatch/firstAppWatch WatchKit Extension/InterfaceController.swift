68aebdf94b54afb363c05bb86ca4b6ba79757104
FelipeSlim <liipe.frs@gmail.com>
2017-09-04 18:44:59 -0300

    Countries sound is working.

--- firstAppWatch/firstAppWatch WatchKit Extension/InterfaceController.swift
+++ firstAppWatch/firstAppWatch WatchKit Extension/InterfaceController.swift
@@ -12,8 +12,27 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    var player : WKAudioFilePlayer!
+    var country : Country!
+    
+    @IBAction func playSound() {
+        player.play()
+    }
+    @IBAction func goToQuiz() {
+        player.pause()
+        self.pushController(withName: "QuizInterfaceController", context: country)
+    }
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
+        
+        var randowIndex = Int(arc4random_uniform(UInt32(4)))
+        country = Country(rawValue: randowIndex)!
+        
+        let filePath = Bundle.main.path(forResource: country.anthem, ofType: "mp3")
+        let fileUrl = NSURL.fileURL(withPath: filePath!)
+        let asset = WKAudioFileAsset(url: fileUrl)
+        var playerItem = WKAudioFilePlayerItem(asset: asset)
+         self.player = WKAudioFilePlayer(playerItem: playerItem)
         
         // Configure interface objects here.
     }
