98cf76731483d83b306367c4cd7c2ce9c2ef8b8e
FelipeSlim <liipe.frs@gmail.com>
2017-09-05 14:42:55 -0300

    updated interface.

--- firstAppWatch/firstAppWatch WatchKit Extension/InterfaceController.swift
+++ firstAppWatch/firstAppWatch WatchKit Extension/InterfaceController.swift
@@ -22,6 +22,8 @@ class InterfaceController: WKInterfaceController {
         player.pause()
         self.pushController(withName: "QuizInterfaceController", context: country)
     }
+    
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
